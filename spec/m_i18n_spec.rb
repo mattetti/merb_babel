bfd52486b60abdf6762e7e88b61735561d78ee73
Matt Aimonetti <mattaimonetti@gmail.com>
2008-05-22 23:15:41 -0700

    fixed rake specs

--- spec/m_i18n_spec.rb
+++ spec/m_i18n_spec.rb
@@ -4,16 +4,16 @@ describe '#localize' do
   
   before(:each) do
     Merb::Controller.send :include, Merb::GlobalHelpers
+    @c = dispatch_to(TestController, :index)
+    @c.add_localization_dir(File.expand_path(File.dirname(__FILE__) + "/lang"))
+    @c.load_localization!
   end
 
   it "should localize a word in english " do
-    c = dispatch_to(TestController, :index) do |c|
-      c.locale.should == 'en-US'
-      c.language.should == 'en'
-      c.country.should == 'US'
-      c.localize(:left).should == 'left'
-    end
-
+      @c.locale.should == 'en-US'
+      @c.language.should == 'en'
+      @c.country.should == 'US'
+      @c.localize(:left).should == 'left'
   end
   
 end
